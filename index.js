f304c0a025a7483785c4f70be125ff11b8bb61b6
Shadique Ali <66332526+shadiqueali@users.noreply.github.com>
2023-01-01 01:14:21 +0530

    add port

--- index.js
+++ index.js
@@ -31,11 +31,15 @@ app.post('/api/upload',upload.single("file"),(req,res)=>{
     res.status(200).json("File has been uploaded")
 })
 
+
+
 app.use('/api/auth',authRoute)
 app.use('/api/users',userRoute) 
 app.use('/api/posts',postRoute)
 app.use('/api/categories',categoryRoute)
 
-app.listen('5000',()=>{
+const port = process.env.PORT || 5000;
+
+app.listen(port,()=>{
     console.log('running my app');
 })
